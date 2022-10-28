da934dde18c7e457e55db221118b56935d3d9db8
marlenebuelt <marlene.bueltemann@student.htw-berlin.de>
2022-10-28 14:50:03 +0200

    slight changes to the contructor due to errors

--- stocks/src/main/java/de/stockexchange/stocks/web/SharesRestController.java
+++ stocks/src/main/java/de/stockexchange/stocks/web/SharesRestController.java
@@ -1,7 +1,6 @@
 package de.stockexchange.stocks.web;
 
 import de.stockexchange.stocks.web.api.Share;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +14,11 @@ public class SharesRestController {
 
     public SharesRestController(){
         shares = new ArrayList<>();
-        shares.add(new Share("865985", "Apple", 147.3, 83000000, 19400000, 0.8203, false));
-        shares.add(new Share("A14Y6F", "Google", 94.41, 257488, 78565, 0.2996, true));
+        shares.add(new Share("865985", "Apple", 147.3,false));
+        shares.add(new Share("A14Y6F", "Google", 94.41, true));
     }
 
-    @GetMapping(path = "/api/v1/stocks")
+    @GetMapping(path = "/api/v1/shares")
     public ResponseEntity<List<Share>> fetchShares(){
         return ResponseEntity.ok(shares);
     }
--- stocks/src/main/java/de/stockexchange/stocks/web/api/Share.java
+++ stocks/src/main/java/de/stockexchange/stocks/web/api/Share.java
@@ -5,18 +5,12 @@ public class Share {
     private String wkn;
     private String name;
     private double stocksPrice;
-    private double revenue;
-    private double costs;
-    private double liabilitiesToEquity;
     private boolean buy;
 
-    public Share(String wkn, String name, double stocksPrice, float companyTurnover, float costs, double liabilitiesToEquity, boolean buy) {
+    public Share(String wkn, String name, double stocksPrice, boolean buy) {
         this.wkn = wkn;
         this.name = name;
         this.stocksPrice = stocksPrice;
-        this.revenue = companyTurnover;
-        this.costs = costs;
-        this.liabilitiesToEquity = liabilitiesToEquity;
         this.buy = buy;
     }
 
@@ -42,30 +36,6 @@ public class Share {
 
     public void setStocksPrice(float stocksPrice) {
         this.stocksPrice = stocksPrice;
-    }
-
-    public double getRevenue() {
-        return revenue;
-    }
-
-    public void setRevenue(float revenue) {
-        this.revenue = revenue;
-    }
-
-    public double getCosts() {
-        return costs;
-    }
-
-    public void setCosts(float costs) {
-        this.costs = costs;
-    }
-
-    public double getLiabilitiesToEquity() {
-        return liabilitiesToEquity;
-    }
-
-    public void setLiabilitiesToEquity(float liabilitiesToEquity) {
-        this.liabilitiesToEquity = liabilitiesToEquity;
     }
 
     public boolean isBuy() {
