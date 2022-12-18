479dfd52268794e4c93ebdcf69796d714756c346
marlenebuelt <marlene.bueltemann@student.htw-berlin.de>
2022-12-18 23:07:53 +0100

    added backend tests

---
+++ src/test/java/de/stockexchange/stocks/service/SharesServiceTest.java
@@ -0,0 +1,69 @@
+package de.stockexchange.stocks.service;
+
+import de.stockexchange.stocks.persistance.SharesEntity;
+import de.stockexchange.stocks.persistance.SharesRepository;
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class SharesServiceTest implements WithAssertions {
+
+    @Mock
+    private SharesRepository sharesRepository;
+    @InjectMocks
+    private SharesService sharesService;
+
+    @Test
+    @DisplayName("Returns true if delete was successful")
+    void returns_true_if_delete_successful() {
+        Long idActual = 1L;
+        doReturn(true).when(sharesRepository).existsById(idActual);
+
+        boolean actual = sharesService.deleteById(idActual);
+
+        verify(sharesRepository).deleteById(idActual);
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    @DisplayName("Returns false if delete failed")
+    void returns_falls_if_delete_failed() {
+        Long idActual = 1L;
+        doReturn(false).when(sharesRepository).existsById(idActual);
+
+        boolean actual = sharesService.deleteById(idActual);
+
+        verifyNoMoreInteractions(sharesRepository);
+        assertThat(actual).isFalse();
+    }
+    @Test
+    @DisplayName("Checks if an item is returned correctly")
+    void test_find_by_id() {
+        var sharesEntity = Mockito.mock(SharesEntity.class);
+        doReturn(1L).when(sharesEntity).getId();
+        doReturn("Apple").when(sharesEntity).getName();
+        doReturn(BigDecimal.valueOf(100.50)).when(sharesEntity).getStocksPrice();
+        doReturn("123456").when(sharesEntity).getWkn();
+        doReturn(true).when(sharesEntity).isBuy();
+
+
+    }
+}
---
+++ src/test/java/de/stockexchange/stocks/service/SharesTransformerTest.java
@@ -0,0 +1,36 @@
+package de.stockexchange.stocks.service;
+
+import de.stockexchange.stocks.persistance.SharesEntity;
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.doReturn;
+
+public class SharesTransformerTest implements WithAssertions {
+
+    private final SharesTransformer sharesTransformer = new SharesTransformer();
+
+    @Test
+    @DisplayName("transforms a shareentity into a share")
+    void transform_shareEntity_into_share(){
+        var sharesEntity = Mockito.mock(SharesEntity.class);
+        doReturn(1L).when(sharesEntity).getId();
+        doReturn("Apple").when(sharesEntity).getName();
+        doReturn(BigDecimal.valueOf(100.50)).when(sharesEntity).getStocksPrice();
+        doReturn("123456").when(sharesEntity).getWkn();
+        doReturn(true).when(sharesEntity).isBuy();
+
+        var actual = sharesTransformer.transformEntity(sharesEntity);
+
+        assertThat(actual.getId()).isEqualTo(1);
+        assertThat(actual.getName()).isEqualTo("Apple");
+        assertThat(actual.getStocksPrice()).isEqualTo(BigDecimal.valueOf(100.50));
+        assertThat(actual.getId()).isEqualTo(1);
+        assertThat(actual.isBuy()).isEqualTo(true);
+    }
+}
